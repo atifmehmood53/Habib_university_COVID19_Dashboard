26577bbf1a5bf8a581d4bd4d645716216e69ff61
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2020-04-02 03:28:29 +0500

    added pip file and DailyCases Model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
+province_choices = [("SINDH", "Sindh"), ("PUNJAB", "Punjab")]
+
 # Create your models here.
-def Daily_Cases(models.Model)
+class Daily_Cases(models.Model):
+    province = models.CharField(choices=province_choices)
+    date = models.DateField( auto_now=False, auto_now_add=False)
+    total_suspected = models.IntegerField(default=0)
+    total_tested = models.IntegerField(default=0)
+    total_tested_positive = models.IntegerField(default=0)
+    total_tested_negative = models.IntegerField(default=0)
+    total_admitted = models.IntegerField(default=0)
+    total_discharged = models.IntegerField(default=0)
+    total_died = models.IntegerField(default=0)
+
+
