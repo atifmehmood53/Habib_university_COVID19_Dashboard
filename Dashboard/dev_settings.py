9477f13e9bbd000bfa662b5abb951daa2c90cd43
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2020-05-02 19:25:18 +0500

    Static files setup accordingt to heroku docs

--- Dashboard/dev_settings.py
+++ Dashboard/dev_settings.py
@@ -3,6 +3,10 @@ from .base_settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'v@467_p0za*-!+^2(97g374733858345snsqg5+du0xvap@aijflh7x1zs5@y6l'
 
