78ede1753f740bf3e2b3fcd5766068f8ef1a40f3
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2020-05-02 18:57:35 +0500

    updated settings

--- Dashboard/wsgi.py
+++ Dashboard/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-env_path = '../.env'
 
-load_dotenv(dotenv_path=env_path)
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dashboard.dev_settings')
+
 application = get_wsgi_application()
