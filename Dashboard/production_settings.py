fadb44d10b4fe59565724eb2fd91a5571511e6c6
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2020-05-04 01:13:03 +0500

    updated database settings

--- Dashboard/production_settings.py
+++ Dashboard/production_settings.py
@@ -12,8 +12,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = 'v@467_p0za*-!+^2(97g374adasyyjhsyjfsdj733858345snsqg5+safh@aijflh7x1zs5@y6l'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 print("Using production environment...")
