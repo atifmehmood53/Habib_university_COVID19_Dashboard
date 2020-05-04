07e052e2ae88f596727de43b910625ba60d39a44
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-05-05 00:33:18 +0500

    fixed names

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dashboard.dev_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dashboard.production_settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
