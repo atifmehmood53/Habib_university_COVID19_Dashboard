eeffb7ded96a0d39e3374e7e27bf9860755b8a41
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-05-06 03:30:56 +0500

    fixed minor front end bugs

--- mainapp/static/mainapp/js/script.js
+++ mainapp/static/mainapp/js/script.js
@@ -690,7 +690,7 @@ function update_timeSeries(chart, data){
 	slider_data[2] = new_data[2]
 	slider_data[3] = new_data[3]
 	copy_b = JSON.parse(JSON.stringify(slider_data));
-	
+	chart.clear()
 	chart.update();
 }
 function update_donutData(chart, data) {
@@ -926,7 +926,7 @@ function startIntro(){
 //var prev_val = 0;
 //var len = b[0].length;
 //console.log('length', len)
-trend_slider.max = len-6;
+trend_slider.max = len-7;
 trend_slider.oninput = function() {
     var val = parseInt(this.value);
 	//console.log(val)
