5a919fea581381c9db5dbadb58a8e3c238899496
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-05-06 02:42:38 +0500

    fixed minor front end bugs

--- mainapp/static/mainapp/js/script.js
+++ mainapp/static/mainapp/js/script.js
@@ -926,7 +926,7 @@ function startIntro(){
 //var prev_val = 0;
 //var len = b[0].length;
 //console.log('length', len)
-trend_slider.max = len-7;
+trend_slider.max = len-6;
 trend_slider.oninput = function() {
     var val = parseInt(this.value);
 	//console.log(val)
