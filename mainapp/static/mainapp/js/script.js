153715b191523739c4eb87cc9bc9371b4032bc44
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-05-06 17:51:57 +0500

    fixed issues

--- mainapp/static/mainapp/js/script.js
+++ mainapp/static/mainapp/js/script.js
@@ -934,7 +934,7 @@ function startIntro(){
 //var prev_val = 0;
 //var len = b[0].length;
 //console.log('length', len)
-trend_slider.max = len-6;
+trend_slider.max = len-7;
 trend_slider.oninput = function() {
     var val = parseInt(this.value);
 	//console.log(val)
@@ -953,6 +953,8 @@ trend_slider.oninput = function() {
 			}
 		}
 	}
+	console.log(slider_data);
+	console.log(copy_b)
 	updateData(myChart, copy_b, 'time series');
 	prev_val = val;	
 }
