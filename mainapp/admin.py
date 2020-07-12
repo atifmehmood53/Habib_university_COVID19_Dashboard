9d88d36a81b66bb2064925f5c50956b9c52c0903
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2020-07-12 22:02:11 +0500

    Update admin.py

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,5 +3,12 @@ from .models import *
 
 # Register your models here.
 admin.site.register([
-    Daily_Cases, Prediction_model, feedback, 
+    Daily_Cases,
+    Prediction_model,
+    feedback,
+    Balochistan_Data,
+    KPK_Data,
+    Sindh_Data,
+    Punjab_Data,
+    Dynamic_Data,
 ])
